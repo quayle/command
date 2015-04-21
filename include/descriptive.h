995039163b27983555d71ec9ad8cecf4e70fd229
Rafał Długołęcki <rafal@dlugolecki.net.pl>
2015-04-22 00:35:08 +0200

    Add descriptive behaviour for command arguments.

---
+++ include/descriptive.h
@@ -0,0 +1,33 @@
+#ifndef __COMMAND_DESCRIPTIVE_H
+#define __COMMAND_DESCRIPTIVE_H
+
+#include <string>
+
+namespace command {
+    /**
+     * Descriptive behaviour class.
+     */
+    class Descriptive {
+        std::string description;
+    public:
+        /**
+         * Default constructor.
+         *
+         * @param description Description
+         */
+        Descriptive(std::string description)
+            : description(description) {
+        }
+
+        /**
+         * Returns description of the current class.
+         *
+         * @return provided description for the class
+         */
+        std::string describe() {
+            return description;
+        }
+    };
+}
+
+#endif /* __COMMAND_DESCRIPTIVE_H */

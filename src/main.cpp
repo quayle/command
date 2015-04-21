11d02293868800d7f5e31b5097e6f0bab0dbf9bc
Rafał Długołęcki <rafal@dlugolecki.net.pl>
2015-04-22 00:16:50 +0200

    Saving designed syntax

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 
 int main() {
+    command::Command command(argc, argv, {
+        command::Option<std::string>("f", "File path", [](std::string value)->void { cout << "Sth: " << value << endl; }),
+        command::Argument<std::string>("File path", []()->void { cout << "Sth: " << value << endl; }),
+        command::Option<void>("help", "Help description", [](void)->void { cout << "Sth: " << value << endl; }),
+        command::Option<void>("verbose", "Verbose option description", &myClass->verbose)
+    });
+
     return 0;
 }
